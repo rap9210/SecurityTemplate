590c0441f00b27c6e4be374fa5f5a857a7906c72
rap9210 <rap9210@gmail.com>
2020-06-10 11:01:06 -0400

    SecurityTemplate

---
+++ src/main/java/com/example/demo/ThSbH2JpaJdbcSecurityTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThSbH2JpaJdbcSecurityTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThSbH2JpaJdbcSecurityTemplateApplication.class, args);
+    }
+
+}
